bf2497afa42462716efe585c2276850a6bf1e474
Tom Cloos <tomcloos@hotmail.com>
2023-12-05 20:15:19 +0100

    Challenge 8

---
+++ Challenge 8/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_8
+{
+    internal class Card
+    {
+        public List<int> Winning { get; set; } = new List<int>();
+        public List<int> Numbers { get; set; } = new List<int>();
+
+        public int Won()
+        {
+            var rslt = 0;
+
+            foreach (var n in Numbers)
+            {
+                if (Winning.Contains(n))
+                {
+                    rslt++;
+                }
+            }
+
+            return rslt;
+        }
+
+        public int Val()
+        {
+            var rslt = 0;
+
+            foreach(var n in Numbers)
+            {
+                if (Winning.Contains(n))
+                {
+                    if (rslt == 0)
+                        rslt = 1;
+                    else
+                        rslt *= 2;
+                }
+            }
+
+            return rslt;
+        }
+    }
+}
---
+++ Challenge 8/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Reflection;
+
+namespace Challenge_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_8.Input-Dummy.txt");
+            var cards = Parse(stream);
+
+            Console.WriteLine(CalculateTotal(cards));
+
+            stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_8.Input.txt");
+            cards = Parse(stream);
+
+            Console.WriteLine(CalculateTotal(cards));
+
+            Console.ReadLine();
+        }
+
+        static int CalculateTotal(List<Card> cards)
+        {
+            var arr = new int[cards.Count];
+
+            for (var i = 0; i < cards.Count; i++)
+            {
+                var c = cards[i];
+                arr[i] += 1;
+                for (var j = 0; j < c.Won(); j++)
+                {
+                    arr[i + j + 1] += arr[i];
+                }
+            }
+
+            return arr.Sum();
+        }
+
+        static List<Card> Parse(Stream stream)
+        {
+            var rslt = new List<Card>();
+
+            using var sr = new StreamReader(stream);
+
+            while (sr.Peek() > -1)
+            {
+                var line = sr.ReadLine();
+                rslt.Add(Parse(line));
+            }
+
+            return rslt;
+        }
+
+        static Card Parse(string line)
+        {
+            var rslt = new Card();
+
+            var start = line.IndexOf(":") + 1;
+            line = line.Substring(start);
+            
+            var split = line.IndexOf("|");
+            var winningString = line.Substring(0, split).Trim();
+            var numberString = line.Substring(split + 1).Trim();
+
+            rslt.Winning
+                .AddRange(winningString.Split(" ")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => int.Parse(s)));
+            rslt.Numbers
+                .AddRange(numberString.Split(" ")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => int.Parse(s)));
+
+            return rslt;
+        }
+    }
+}
