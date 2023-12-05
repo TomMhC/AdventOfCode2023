ff70ff68781d72c5a769ae3d2fdddbe9cdb5dc5c
Tom Cloos <tomcloos@hotmail.com>
2023-12-05 20:09:37 +0100

    Challenge 7

---
+++ Challenge 7/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_7
+{
+    internal class Card
+    {
+        public List<int> Winning { get; set; } = new List<int>();
+        public List<int> Numbers { get; set; } = new List<int>();
+        public int Val()
+        {
+            var rslt = 0;
+
+            foreach(var n in Numbers)
+            {
+                if (Winning.Contains(n))
+                {
+                    if (rslt == 0)
+                        rslt = 1;
+                    else
+                        rslt *= 2;
+                }
+            }
+
+            return rslt;
+        }
+    }
+}
---
+++ Challenge 7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+
+namespace Challenge_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_7.Input-Dummy.txt");
+            var cards = Parse(stream);
+
+            Console.WriteLine(cards.Sum(c => c.Val()));
+
+            stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_7.Input.txt");
+            cards = Parse(stream);
+
+            Console.WriteLine(cards.Sum(c => c.Val()));
+
+            Console.ReadLine();
+        }
+
+        static List<Card> Parse(Stream stream)
+        {
+            var rslt = new List<Card>();
+
+            using var sr = new StreamReader(stream);
+
+            while (sr.Peek() > -1)
+            {
+                var line = sr.ReadLine();
+                rslt.Add(Parse(line));
+            }
+
+            return rslt;
+        }
+
+        static Card Parse(string line)
+        {
+            var rslt = new Card();
+
+            var start = line.IndexOf(":") + 1;
+            line = line.Substring(start);
+            
+            var split = line.IndexOf("|");
+            var winningString = line.Substring(0, split).Trim();
+            var numberString = line.Substring(split + 1).Trim();
+
+            rslt.Winning
+                .AddRange(winningString.Split(" ")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => int.Parse(s)));
+            rslt.Numbers
+                .AddRange(numberString.Split(" ")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => int.Parse(s)));
+
+            return rslt;
+        }
+    }
+}
