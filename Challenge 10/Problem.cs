83997bad84230f8f2d362e205be39f30e422f6d5
Tom Cloos <tomcloos@hotmail.com>
2023-12-05 22:09:00 +0100

    Challenge 10

---
+++ Challenge 10/Problem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_10
+{
+    internal class Problem
+    {
+        public List<Map> Maps { get; set; } = new List<Map>();
+
+        public List<Tuple<Int64, Int64>> Seeds { get; set; } = new List<Tuple<Int64, Int64>>(); 
+    }
+}
---
+++ Challenge 10/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Reflection;
+
+namespace Challenge_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_10.Input-Dummy.txt");
+            var problem = Parse(stream);
+
+            Console.WriteLine(CalculateLowestLocation(problem));
+
+            stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Challenge_10.Input.txt");
+            problem = Parse(stream);
+
+            Console.WriteLine("Done!");
+            Console.WriteLine(CalculateLowestLocation(problem));
+
+            Console.ReadLine();
+        }
+
+        static Int64 CalculateLowestLocation(Problem problem)
+        {
+            var minimum = Int64.MaxValue;
+
+            foreach (var tpl in problem.Seeds)
+            {
+                Console.WriteLine(tpl.ToString());
+
+                for (var i = 0; i < tpl.Item2; i++)
+                {
+                    var x = tpl.Item1 + i;
+
+                    foreach (var m in problem.Maps)
+                    {
+                        x = m.GetDestination(x);
+                    }
+
+                    if (x < minimum)
+                        minimum = x;
+                }
+            }
+
+            return minimum;
+        }
+
+        static List<Tuple<Int64, Int64>> Parse(string seedLine)
+        {
+            var rslt = new List<Tuple<Int64, Int64>>();
+
+            seedLine = seedLine.Substring(seedLine.IndexOf(":") + 1).Trim();
+
+            var numbersS = seedLine.Split(" ");
+
+            var numbers = numbersS.Select(n => Int64.Parse(n)).ToList();
+
+            for(var i = 0; i < numbers.Count; i+=2)
+            {
+                rslt.Add(new Tuple<Int64, Int64>(numbers[i], numbers[i + 1]));
+            }
+
+            return rslt;
+        }
+
+        static Problem Parse(Stream stream)
+        {
+            var rslt = new Problem();
+
+            using var sr = new StreamReader(stream);
+
+            var seedLine = sr.ReadLine();
+
+            rslt.Seeds = Parse(seedLine);
+
+            sr.ReadLine();
+
+            var lines = new List<string>();
+            while (sr.Peek() > -1)
+            {
+                var line = sr.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    var map = new Map();
+                    map.Parse(lines);
+                    rslt.Maps.Add(map);
+
+                    lines.Clear();
+                }
+                else
+                {
+                    if (char.IsDigit(line[0]))
+                        lines.Add(line);
+                }
+            }
+
+            var map2 = new Map();
+            map2.Parse(lines);
+            rslt.Maps.Add(map2);
+
+            return rslt;
+        }
+    }
+}
